70554fbd247998895b0edb2148139916ee18200f
Biswadip Mandal <biswadip.iitb@gmail.com>
2021-11-22 22:16:01 -0600

    cpin

--- V6filesys_new.c
+++ V6filesys_new.c
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 
 // CONSTANTS
 #define BLOCK_SIZE 1024
@@ -24,10 +25,13 @@
 #define null_size 1024 // * check where it's getting used
 #define FREE_BLOCKS 251
 #define DIRECTORY_SIZE 32
+#define MAX_FILE_TOKENS 28
+#define INODE_ADDRESSES 9
 
 // VARIABLES
 int file_handle;
 unsigned short block_number_tracker[max_array];
+char current_file[200];
 
 // super block
 typedef struct {
@@ -48,7 +52,7 @@ unsigned int uid;
 unsigned int gid;
 unsigned int size0;
 unsigned int size1;
-unsigned int addr[9];
+unsigned int addr[INODE_ADDRESSES];
 unsigned int actime;
 unsigned int modtime;
 } inode_type;
@@ -57,21 +61,64 @@ inode_type first_inode;
 // directory
 typedef struct { 
     unsigned int inode; 
-    char filename[28];
+    char filename[MAX_FILE_TOKENS];
 } dir_type;
 
 dir_type new_directory;
+
+// cpin externalfile v6-file
+
+
+
+// initialize current = "";
+// 1. main a relative path. check if absolute path or relative path - done
+//        1.1 if absolute then go to 2.
+//        1.2 if relative absolute = current_file + relative
+// 2. check_if_directory_exists(directory)
+// 3. read_file(file)
+// 4. write_file(file, bytes)
+// 5. 
+
+
+// cpout v6-file externalfile
+
 
 
 void chaining_blocks(unsigned int total_blocks, unsigned int inode_block_count);
 void create_root_dir();
 int initialize_file_system(char *path, unsigned int total_blocks, unsigned int total_inodes_blocks);
 unsigned int get_free_block();
-void read_block(unsigned short *dest, unsigned short bno);
+void read_block(unsigned short *dest, unsigned int bno);
 void write_block(unsigned int *dest, unsigned int bno);
-void write_inode(inode_type current_inode, unsigned int inode_index);
+void write_inode(inode_type* current_inode, unsigned int inode_index);
+bool check_absolute_path(char *path);
+
+
+void create_file_inode(char *path);   // create inode for file
+void create_dir_inode(char *path);    // create inode for dire
+char** tokenize_file_path(char *filepath, int *token_count, int max_tokens);
+unsigned int cpin_handle(char *external_file, char *v6_file);
+void read_external_file(char *path);
+unsigned int get_inode_no_for_directory(inode_type *current_inode, char *filetoken);
+
+void bytes_to_directory(char *data, dir_type *direc);
+
+// splitfile and check existance
 
 //in order
+
+bool check_nth_bit(short a, short n){
+    if((short) (a & (1 << n)) != 0){
+        return true;
+    }
+    return false;
+}
+
+int min(int x, int y)
+{
+  return (x < y) ? x : y;
+}
+
 
 // Data blocks chaining procedure
 void chaining_blocks(unsigned int total_blocks, unsigned int inode_block_count)
@@ -86,7 +133,7 @@ void chaining_blocks(unsigned int total_blocks, unsigned int inode_block_count)
 
     unsigned int data_blocks_count = (total_blocks - inode_block_count - 2);
 
-    unsigned int split_blocks =  data_blocks_count / FREE_BLOCKS;           //TODO check 
+    unsigned int split_blocks =  data_blocks_count / FREE_BLOCKS;           //TODO: check 
     unsigned short extra_blocks = data_blocks_count % FREE_BLOCKS;
     unsigned short idx = 0;
 
@@ -147,6 +194,7 @@ void chaining_blocks(unsigned int total_blocks, unsigned int inode_block_count)
         }
     }
 }
+
 
 
 //create root directory and inode.
@@ -273,8 +321,41 @@ unsigned int get_free_block()
     return next_block;
 }
 
+
 //Read integer array from the required block&&
-void read_block(unsigned short *dest, unsigned short block_number)
+void read_inode(unsigned short *dest, unsigned short inode_number)
+{
+
+    int flag = 0;
+    if (inode_number > super.isize * BLOCK_SIZE / INODE_SIZE)
+        flag = 1;
+    else
+    {
+        lseek(file_handle, inode_number * INODE_SIZE, SEEK_SET); //added block size
+        read(file_handle, dest, INODE_SIZE);
+    }
+}
+
+
+dir_type* read_directory_type(unsigned int address)
+{
+    dir_type *direc;
+    direc = malloc(sizeof(dir_type));
+    printf("direc size %\n", sizeof(direc));
+    char direc_bytes[DIRECTORY_SIZE];
+    printf("reading directory %d\n", address);
+    lseek(file_handle, address, SEEK_SET); 
+    printf("reading directory\n");  
+    read(file_handle, &direc_bytes, DIRECTORY_SIZE);
+    printf("reading directory\n");
+    bytes_to_directory(&direc_bytes, direc);
+    printf("reading directory\n");
+    return direc;
+}
+
+
+//Read integer array from the required block&&
+void read_block(unsigned short *dest, unsigned int block_number)
 {
     int flag = 0;
     if (block_number > super.isize + super.fsize)
@@ -310,14 +391,328 @@ void write_block(unsigned int *dest, unsigned int bno)
 }
 
 //Write to an inode given the inode number	&&
-void write_inode(inode_type current_inode, unsigned int inode_index)
+void write_inode(inode_type* current_inode, unsigned int inode_index)
 {
     int num_bytes;
     lseek(file_handle, 2 * BLOCK_SIZE + inode_index * INODE_SIZE, 0);
-    num_bytes = write(file_handle, &current_inode, INODE_SIZE);
+    num_bytes = write(file_handle, current_inode, INODE_SIZE);
 
     if ((num_bytes) < INODE_SIZE)
         printf("\n Error in inode number : %d\n", inode_index);
+}
+
+bool check_absolute_path(char *path){
+    if (path[0] == '/'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+char** tokenize_file_path(char *filepath, int *token_count, int max_tokens){
+
+    char **file_tokens = malloc(sizeof(char *) * max_tokens);
+
+    int i = 0;
+    char * file = strtok(filepath, "/");
+
+    while(file!=NULL){
+        file_tokens[i] = file;
+        i ++;
+        file = strtok(NULL, "/");
+    }
+
+    *token_count = i;
+    return file_tokens;
+}
+
+
+unsigned int get_inode_no_for_directory(inode_type *current_inode, char *filetoken){
+    // get inode number in the current inode where filetoken is present
+    unsigned int addresses[INODE_ADDRESSES];
+    memcpy( &addresses, current_inode->addr, INODE_ADDRESSES*4);
+    unsigned int block_number;
+    char block_data[BLOCK_SIZE];
+    unsigned int inode_number = 0;
+    char inode_filename[28];
+
+    for (int i = 0; i < INODE_ADDRESSES; i++){
+        block_number = addresses[i];
+        read_block(block_data, block_number);
+
+        for (int j = 0;j < BLOCK_SIZE / DIRECTORY_SIZE ; j++ ){
+            memcpy(&inode_number, &block_data[j * DIRECTORY_SIZE ], 4);
+            memcpy(&inode_filename, &block_data[j * DIRECTORY_SIZE + 4 ], 28);
+
+            if (inode_number > 0){
+                if (strncmp(inode_filename, filetoken, 28) == 0){
+                    return inode_number;
+                }
+            }
+
+        }
+
+    }
+    return 0;
+}
+
+void bytes_to_inode(char *data, inode_type *inode) {
+    memcpy(&inode->flags, &data[0], 2);
+    memcpy(&inode->nlinks, &data[2], 2);
+    memcpy(&inode->uid, &data[4], 4);
+    memcpy(&inode->gid, &data[8], 4);
+    memcpy(&inode->size0, &data[12], 4);
+    memcpy(&inode->size1, &data[16], 4);
+    memcpy(&inode->addr, &data[20], 36);
+    memcpy(&inode->actime, &data[56], 4);
+    memcpy(&inode->modtime, &data[60], 4);
+}
+
+
+void inode_to_bytes(inode_type *inode, uint8_t *data) {
+    memcpy(&data[0], &inode->flags, 2);
+    memcpy(&data[2], &inode->nlinks, 2);
+    memcpy(&data[4], &inode->uid, 4);
+    memcpy(&data[8], &inode->gid, 4);
+    memcpy(&data[12], &inode->size0, 4);
+    memcpy(&data[16], &inode->size1, 4);
+    memcpy(&data[20], &inode->addr, 36);
+    memcpy(&data[56], &inode->actime, 4);
+    memcpy(&data[60], &inode->modtime, 4);
+}
+
+void bytes_to_directory(char *data, dir_type *direc) {
+    memcpy(&direc->inode, &data[0],  4);
+    memcpy(&direc->filename, &data[4], 28);
+}
+
+
+unsigned int get_free_inode_number(superblock_type *super_block){
+    // checking for the free available free inode.
+    // nlinks is checked if the inode is valid or not
+    int inode_number = 1;
+    int block_number = 2;
+    int nlinks;
+
+    while(block_number < super_block->isize + 2){
+        read_block(block_number_tracker, block_number);
+        for (int j = 0; j < BLOCK_SIZE / INODE_SIZE ; j++){
+            nlinks = block_number_tracker[j * INODE_SIZE / 2 + 1];
+            if (nlinks == 0){
+                return inode_number;
+            }
+            inode_number ++;
+        }
+    }
+    return 0;
+}
+
+inode_type* get_inode_by_number(unsigned int inode_number){
+    inode_type *inode;
+    inode = malloc(sizeof(inode_type));
+    char inode_data[INODE_SIZE];
+    read_inode(inode_data, inode_number);
+    bytes_to_inode(&inode_data, inode);
+    return inode;
+}
+
+
+
+
+
+bool is_inode_directory(inode_type *inode){
+    bool b = check_nth_bit(inode->flags, 14);
+    bool c = check_nth_bit(inode->flags, 13);
+    return b && !c;
+}
+
+
+void fill_file_inode(inode_type *file_inode){
+    // small file
+    file_inode->flags = 0b1000110000000000;
+    file_inode->nlinks = 1;
+    file_inode->uid = 1;                            
+    file_inode->gid = 1;
+    file_inode->size0 = 0;
+    file_inode->size1 = 0;
+    file_inode->actime = time(0);
+    file_inode->modtime = time(0);
+}
+
+void fill_directory_inode(inode_type *dir_inode){
+    // b, c is 1,0
+    dir_inode->flags = 0b1100110000000000;
+    dir_inode->nlinks = 2;
+    dir_inode->uid = 1;                            
+    dir_inode->gid = 1;
+    dir_inode->size0 = 0;
+    dir_inode->size1 = 0;
+    dir_inode->actime = time(0);
+    dir_inode->modtime = time(0);
+}
+
+
+unsigned int add_directory_to_inode(inode_type *inode, char *filename, unsigned int inode_number){
+
+    if(is_inode_directory(inode) == false){
+        printf("\nFailed to save directory type for %s\n", filename);
+        return -1;
+    }
+
+    dir_type new_directory;
+    new_directory.inode = inode_number;
+    strncpy(new_directory.filename, filename, min(sizeof(filename), MAX_FILE_TOKENS));
+
+    dir_type *current_dir;
+
+    unsigned int address_block;
+    unsigned short dir_index;
+    unsigned short i;
+
+    for(i=0 ; i < 9 ; i++){
+        address_block = inode->addr[i];
+
+        if(address_block == 0){
+            printf("address block ..%d\n", address_block);
+            address_block = get_free_block();
+            lseek(file_handle, address_block * BLOCK_SIZE, SEEK_SET);
+            write(file_handle, &new_directory, DIRECTORY_SIZE);
+            inode->addr[i] = address_block;
+            return 0;
+        }
+
+        for (dir_index = 0; dir_index < BLOCK_SIZE / DIRECTORY_SIZE; dir_index++){
+            printf("dir index ..%d\n", dir_index);
+            current_dir = read_directory_type(address_block * BLOCK_SIZE + dir_index * DIRECTORY_SIZE);
+
+            if (current_dir->inode == 0){
+                // store directory
+                lseek(file_handle, address_block * BLOCK_SIZE + dir_index * DIRECTORY_SIZE, SEEK_SET);
+                write(file_handle, &new_directory, DIRECTORY_SIZE);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
+
+unsigned int cpin_handle(char *external_file, char *v6_file){
+    int token_count = 0;
+    
+    printf("External file: %s\n", external_file);
+    printf("V6 File: %s\n", v6_file);
+    bool absolute = check_absolute_path(external_file);
+
+    // TODO: - handle absolute is false
+    if (absolute == false){
+        strcat(current_file, external_file);
+        printf("%s\n", current_file);
+    }
+    char **file_tokens = tokenize_file_path(v6_file, &token_count, 40);
+
+    printf("token count %d\n",token_count);
+
+    inode_type *curr_inode = NULL;
+    inode_type *previous_inode = get_inode_by_number(1);
+    unsigned int current_inode_no = 0;
+    unsigned int previous_inode_no = 1;
+
+    
+    // iterate through the file path
+    // leaving the last token out as inode_no = 0 means that the file exists, 
+    // but for directory it means that directory doesn't exist
+    for (int i = 0 ; i < token_count - 1; i++){
+        current_inode_no = get_inode_no_for_directory(previous_inode, file_tokens[i]);
+
+        if (current_inode_no == 0){
+            
+            // create the folder if it doesn't exists
+            current_inode_no = get_free_inode_number(&super);
+            printf("%s doesn't exists. Creating the folder at inode %d\n", file_tokens[i], current_inode_no);
+            curr_inode =  get_inode_by_number(current_inode_no);
+            fill_directory_inode(curr_inode);
+
+
+            // add . and .. to the current directory
+            add_directory_to_inode(curr_inode, ".", current_inode_no);
+            add_directory_to_inode(curr_inode, "..", previous_inode_no);
+            printf("writing inode..\n");
+            write_inode(curr_inode, current_inode_no);
+
+            // link with parent
+            printf("adding direcctory in parent %s\n", file_tokens[i]);
+            add_directory_to_inode(previous_inode, file_tokens[i], current_inode_no);
+
+            printf("writing inode..\n");
+
+            write_inode(previous_inode, previous_inode_no);
+
+        }else{
+            printf("%s already exists\n", file_tokens[i]);
+            curr_inode = get_inode_by_number(current_inode_no);
+        }
+
+        free(previous_inode);
+        previous_inode = curr_inode;
+        previous_inode_no = current_inode_no;
+    }
+
+
+    // for the last token or file.
+    current_inode_no = get_inode_no_for_directory(curr_inode, file_tokens[token_count - 1]);
+
+    if(current_inode_no == 0){
+        printf("File already exists!!");
+    }else{
+        current_inode_no =  get_free_inode_number(&super);
+        if (current_inode_no == 0){
+            printf("Memory for file system exceeds!!");
+            return 0;
+        }
+        curr_inode = get_inode_by_number(current_inode_no);
+
+        // link with parent
+        add_directory_to_inode(previous_inode, file_tokens[token_count-1], current_inode_no);
+        write_inode(previous_inode, previous_inode_no);
+        printf("File %s doesn't exists. Creating the file at inode %d\n", file_tokens[token_count -1], current_inode_no);
+
+        fill_file_inode(curr_inode);       
+        
+        FILE *f = fopen(external_file, "rb");
+        char data_store[BLOCK_SIZE];
+        int free_block;
+        int address_i = 0;
+        unsigned int total_size = 0;
+
+
+        // for small sizes
+        while(feof(f)==0 || address_i < 9){
+            free_block = get_free_block();
+            if (free_block == 0){
+                return 0;
+            }
+            size_t num_bytes = fread(data_store, 1, BLOCK_SIZE, f);
+            total_size += num_bytes;
+            write_block(data_store, free_block);
+            curr_inode->addr[address_i] = free_block;
+        }
+        curr_inode->size0 = total_size;             // TODO: check how to incorporate size1
+
+        write_inode(curr_inode, current_inode_no);
+        
+    }
+
+
+    // eheck if directory exists for file -- assume it exists for now
+    // read external file content  -- 
+    // create new i node for file
+    //      get datablock and put value
+    //      set flags and size
+    //      no need for directory as only dealing with file 
+    //      setting nlinks?
+    return 0;
+
 }
 
 int main(int argc, char *argv[])
@@ -329,6 +724,7 @@ int main(int argc, char *argv[])
     unsigned int bno = 0, inode_block_count = 0;
     char file_path[200];
     char *filename;
+    char *external_file, * v6_file;
     char *num1, *num2;
 
     printf("\n\nInitialize file system by using\n1. openfs <file_name>\n2.initfs < total blocks> < total inode blocks>\n");
@@ -374,12 +770,24 @@ int main(int argc, char *argv[])
                 }
             }
         }
+
+
         else if (strcmp(parser, "openfs") == 0)
         {
             // file_path = strtok(NULL, " "); //read the second breaked tokens
             strcpy(file_path, strtok(NULL, " "));
-            printf("%s", file_path);
+            printf("%s\n", file_path);
         }
+
+
+        else if (strcmp(parser, "cpin") == 0)
+        {
+            external_file = strtok(NULL, " "); 
+            v6_file = strtok(NULL, " "); 
+            cpin_handle(external_file, v6_file);
+        }
+
+
         else if (strcmp(parser, "q") == 0)
         {
             printf("Exiting the program");
@@ -388,3 +796,4 @@ int main(int argc, char *argv[])
         }
     }
 }
+
